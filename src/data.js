4ec1e11af42e3244b4f87603b6c63227fbddceec
Rick Metzger <rickymetz@gmail.com>
2019-09-30 08:46:06 -0400

    Add filler schools data object for react-select

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const schoolsList = [
+ 	{value:'UVA', label: 'University of Virginia'},
+ 	{value:'VT', label: 'Virginia Polytechnic Institute and State University'},
+ 	{value:'GMU', label: 'George Mason University'},
+ 	{value:'LU', label: 'Liberty University'},
+ 	{value:'VCU', label: 'Virginia Commonwealth University'},
+ 	{value:'W&M', label: 'College of William & Mary'},
+ 	{value:'ODU', label: 'Old Dominion University'},
+ 	{value:'JMU', label: 'James Madison University'},
+ 	{value:'UofR', label: 'University of Richmond'},
+ 	{value:'RU', label: 'Radford University'}
+]
