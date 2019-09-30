b0588c3c7c951a960c2c244042926fd083eb0fb4
Rick Metzger <rickymetz@gmail.com>
2019-09-30 09:39:12 -0400

    Schools data object with placeholder descriptions

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export const schoolsList = [
- 	{value:'UVA', label: 'University of Virginia'},
- 	{value:'VT', label: 'Virginia Polytechnic Institute and State University'},
- 	{value:'GMU', label: 'George Mason University'},
- 	{value:'LU', label: 'Liberty University'},
- 	{value:'VCU', label: 'Virginia Commonwealth University'},
- 	{value:'W&M', label: 'College of William & Mary'},
- 	{value:'ODU', label: 'Old Dominion University'},
- 	{value:'JMU', label: 'James Madison University'},
- 	{value:'UofR', label: 'University of Richmond'},
- 	{value:'RU', label: 'Radford University'}
+ 	{value:'UVA', label: 'University of Virginia', desc: 'Something important here (e.g. school enrollment, school location)'},
+ 	{value:'VT', label: 'Virginia Polytechnic Institute and State University', desc: 'Something important here (e.g. school enrollment, school location)'},
+ 	{value:'GMU', label: 'George Mason University', desc: 'Something important here (e.g. school enrollment, school location)'},
+ 	{value:'LU', label: 'Liberty University', desc: 'Something important here (e.g. school enrollment, school location)'},
+ 	{value:'VCU', label: 'Virginia Commonwealth University', desc: 'Something important here (e.g. school enrollment, school location)'},
+ 	{value:'W&M', label: 'College of William & Mary', desc: 'Something important here (e.g. school enrollment, school location)'},
+ 	{value:'ODU', label: 'Old Dominion University', desc: 'Something important here (e.g. school enrollment, school location)'},
+ 	{value:'JMU', label: 'James Madison University', desc: 'Something important here (e.g. school enrollment, school location)'},
+ 	{value:'UofR', label: 'University of Richmond', desc: 'Something important here (e.g. school enrollment, school location)'},
+ 	{value:'RU', label: 'Radford University', desc: 'Something important here (e.g. school enrollment, school location)'}
 ]
 
 export const comparisons = [
