840cd9461af5f91d0296e695ab8ce9c07c508807
Rick Metzger <rickymetz@gmail.com>
2019-10-02 15:21:50 -0400

    Add recharts to project, add to data file for recharts to consume, add filler parallel chart

--- src/data.js
+++ src/data.js
@@ -1,14 +1,44 @@
 export const schoolsList = [
- 	{value:'UVA', label: 'University of Virginia', desc: 'Something important here (e.g. school enrollment, school location)'},
- 	{value:'VT', label: 'Virginia Polytechnic Institute and State University', desc: 'Something important here (e.g. school enrollment, school location)'},
- 	{value:'GMU', label: 'George Mason University', desc: 'Something important here (e.g. school enrollment, school location)'},
- 	{value:'LU', label: 'Liberty University', desc: 'Something important here (e.g. school enrollment, school location)'},
- 	{value:'VCU', label: 'Virginia Commonwealth University', desc: 'Something important here (e.g. school enrollment, school location)'},
- 	{value:'W&M', label: 'College of William & Mary', desc: 'Something important here (e.g. school enrollment, school location)'},
- 	{value:'ODU', label: 'Old Dominion University', desc: 'Something important here (e.g. school enrollment, school location)'},
- 	{value:'JMU', label: 'James Madison University', desc: 'Something important here (e.g. school enrollment, school location)'},
- 	{value:'UofR', label: 'University of Richmond', desc: 'Something important here (e.g. school enrollment, school location)'},
- 	{value:'RU', label: 'Radford University', desc: 'Something important here (e.g. school enrollment, school location)'}
+ 	{value:'UVA', label: 'University of Virginia', desc: 'Something important here (e.g. school enrollment, school location)', data: [
+    {category: 'Unadjusted Scores', score: (Math.random() * 100).toFixed(2)},
+    {category: 'Adjusted Scores', score: (Math.random() * 100).toFixed(2)},
+  ]},
+ 	{value:'VT', label: 'Virginia Polytechnic Institute and State University', desc: 'Something important here (e.g. school enrollment, school location)', data: [
+    {category: 'Unadjusted Scores', score: (Math.random() * 100).toFixed(2)},
+    {category: 'Adjusted Scores', score: (Math.random() * 100).toFixed(2)},
+  ]},
+ 	{value:'GMU', label: 'George Mason University', desc: 'Something important here (e.g. school enrollment, school location)', data: [
+    {category: 'Unadjusted Scores', score: (Math.random() * 100).toFixed(2)},
+    {category: 'Adjusted Scores', score: (Math.random() * 100).toFixed(2)},
+  ]},
+ 	{value:'LU', label: 'Liberty University', desc: 'Something important here (e.g. school enrollment, school location)', data: [
+    {category: 'Unadjusted Scores', score: (Math.random() * 100).toFixed(2)},
+    {category: 'Adjusted Scores', score: (Math.random() * 100).toFixed(2)},
+  ]},
+ 	{value:'VCU', label: 'Virginia Commonwealth University', desc: 'Something important here (e.g. school enrollment, school location)', data: [
+    {category: 'Unadjusted Scores', score: (Math.random() * 100).toFixed(2)},
+    {category: 'Adjusted Scores', score: (Math.random() * 100).toFixed(2)},
+  ]},
+ 	{value:'W&M', label: 'College of William & Mary', desc: 'Something important here (e.g. school enrollment, school location)', data: [
+    {category: 'Unadjusted Scores', score: (Math.random() * 100).toFixed(2)},
+    {category: 'Adjusted Scores', score: (Math.random() * 100).toFixed(2)},
+  ]},
+ 	{value:'ODU', label: 'Old Dominion University', desc: 'Something important here (e.g. school enrollment, school location)', data: [
+    {category: 'Unadjusted Scores', score: (Math.random() * 100).toFixed(2)},
+    {category: 'Adjusted Scores', score: (Math.random() * 100).toFixed(2)},
+  ]},
+ 	{value:'JMU', label: 'James Madison University', desc: 'Something important here (e.g. school enrollment, school location)', data: [
+    {category: 'Unadjusted Scores', score: (Math.random() * 100).toFixed(2)},
+    {category: 'Adjusted Scores', score: (Math.random() * 100).toFixed(2)},
+  ]},
+ 	{value:'UofR', label: 'University of Richmond', desc: 'Something important here (e.g. school enrollment, school location)', data: [
+    {category: 'Unadjusted Scores', score: (Math.random() * 100).toFixed(2)},
+    {category: 'Adjusted Scores', score: (Math.random() * 100).toFixed(2)},
+  ]},
+ 	{value:'RU', label: 'Radford University', desc: 'Something important here (e.g. school enrollment, school location)', data: [
+    {category: 'Unadjusted Scores', score: (Math.random() * 100).toFixed(2)},
+    {category: 'Adjusted Scores', score: (Math.random() * 100).toFixed(2)},
+  ]}
 ]
 
 export const comparisons = [
