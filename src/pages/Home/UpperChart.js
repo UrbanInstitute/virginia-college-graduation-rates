47cc3d219fe212338ac0522c3d5d5b594ddf7051
Sean Perkins <sean@mobility-labs.com>
2019-10-07 18:59:14 -0400

    Increase chart size

--- src/pages/Home/UpperChart.js
+++ src/pages/Home/UpperChart.js
@@ -1,43 +1,58 @@
-import React, {useContext} from 'react'
-import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip,} from 'recharts'
+import React, {Component} from 'react'
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  LabelList,
+} from 'recharts'
 
 import {getSchoolColor} from '../../lib/colors'
 import DataContext from '../../modules/dataContext'
 
 import './UpperChart.scss'
 
-const UpperChart = () => {
-  const {graduation, data, schools} = useContext(DataContext)
-  // Place data into chart format
-  const series = data.map((col) => ({
-    label: col.name,
-    color: getSchoolColor(col.name, schools),
-    data: [
-      {category: 'Unadjusted Scores', score: col[`grad_rate_${graduation}`]},
-      {category: 'Adjusted Scores', score: col[`adj_${graduation}_grad_rate`]},
-    ]
-  }))
-  return (
-    <ResponsiveContainer className="upper-chart">
-      <LineChart>
-        <XAxis dataKey="category" allowDuplicatedCategory={false} axisLine={false} dy={0} />
-        <YAxis yAxisId="line" orientation="left" dataKey="score" domain={[0, 100]} interval="preserveStartEnd" ticks={[0, 100]} tickLine={false} unit="%"/>
-        <YAxis orientation="right" />
-        {
-          series.map((col, i) => (
-            <Line
-              yAxisId="line"
-              dataKey="score"
-              data={col.data}
-              name={col.label}
-              stroke={col.color}
-              key={i}
-            />
-          ))
-        }
-      </LineChart>
-    </ResponsiveContainer>
-  )
+class UpperChart extends Component {
+  render() {
+    const {graduation, data, schools} = this.context
+    // Place data into chart format
+    const series = data.map((col) => ({
+      label: col.name,
+      color: getSchoolColor(col.name, schools),
+      data: [
+        {category: 'Unadjusted Scores', score: col[`grad_rate_${graduation}`]},
+        {category: 'Adjusted Scores', score: col[`adj_${graduation}_grad_rate`]},
+      ]
+    }))
+    return (
+      <ResponsiveContainer className="upper-chart">
+        <LineChart>
+          <XAxis dataKey="category" allowDuplicatedCategory={false} axisLine={false} dy={0} />
+          <YAxis yAxisId="line" orientation="left" dataKey="score" domain={[0, 100]} interval="preserveStartEnd" ticks={[0, 100]} tickLine={false} unit="%"/>
+          <YAxis orientation="right" />
+          {
+            series.map((col, i) => (
+              <Line
+                yAxisId="line"
+                dataKey="score"
+                data={col.data}
+                name={col.label}
+                stroke={col.color}
+                dot={{stroke: col.color, fill: col.color, r: 5}}
+                key={i}
+              />
+            ))
+          }
+        </LineChart>
+      </ResponsiveContainer>
+    )
+  }
 }
 
+UpperChart.contextType = DataContext
+
 export default UpperChart
+
+const renderLabel = (entry) => (entry.name)
