d04ab0978d2b94092455dc09c22e952dc72b1360
Sean Perkins <sean@mobility-labs.com>
2019-10-13 17:46:07 -0400

    Change notebook subtext with institution

--- src/pages/Home/Notebook.js
+++ src/pages/Home/Notebook.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
+import DataContext from '../../modules/dataContext'
 import {TAB_MAPPING} from '../../constants/mappings'
 
 import SchoolSelector from '../../components/SchoolSelector'
@@ -7,24 +8,27 @@ import SchoolChip from '../../components/SchoolChip'
 import LowerChart from './LowerChart'
 import SchoolChips from './SchoolChips'
 
-const Notebook = ({selectedTab}) => (
-  <div className="card" style={{minHeight:700}}>
-    <div className="card-body">
-      <div className="intro-text">
-        <small className="text-uppercase text-primary">4-Year Schools</small>
-        <h3>{TAB_MAPPING[selectedTab]['heading']}</h3>
-        <p>{TAB_MAPPING[selectedTab]['subheading']}</p>
+const Notebook = ({selectedTab}) => {
+  const {institution} = useContext(DataContext)
+  return (
+    <div className="card" style={{minHeight:700}}>
+      <div className="card-body">
+        <div className="intro-text">
+          <small className="text-uppercase text-primary">{institution === '4yr' ? '4' : '2'}-Year Schools</small>
+          <h3>{TAB_MAPPING[selectedTab]['heading']}</h3>
+          <p>{TAB_MAPPING[selectedTab]['subheading']}</p>
+        </div>
+        <div>
+          <SchoolChip
+            label="State average"
+          />
+          <SchoolSelector/>
+          <SchoolChips hideDescription={true} />
+        </div>
+        <LowerChart items={TAB_MAPPING[selectedTab]['items']} />
       </div>
-      <div>
-        <SchoolChip
-          label="State average"
-        />
-        <SchoolSelector/>
-        <SchoolChips hideDescription={true} />
-      </div>
-      <LowerChart items={TAB_MAPPING[selectedTab]['items']} />
     </div>
-  </div>
-)
+  )
+}
 
 export default Notebook
