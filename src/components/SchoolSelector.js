fae75214b8ff2fd87af7d84b9a0f7f595c84e2f7
Sean Perkins <sean@mobility-labs.com>
2019-10-18 13:15:23 -0400

    Closes #4 remove state average from select

--- src/components/SchoolSelector.js
+++ src/components/SchoolSelector.js
@@ -4,8 +4,8 @@ import DataContext from '../modules/dataContext'
 
 import './SchoolSelector.scss'
 
+// Components from react select that we want to customize
 const {ValueContainer, Placeholder} = components
-
 
 class SchoolSelector extends Component {
 
@@ -23,14 +23,14 @@ class SchoolSelector extends Component {
     const {data, schools} = this.context
     const CustomValueContainer = ({children, ...props}) => {
       return (
-        <components.ValueContainer {...props}>
+        <ValueContainer {...props}>
           <Placeholder {...props} isFocused={props.isFocused}>
             {(schools.length > 0) ? (schools.length + ' / 3') : props.selectProps.placeholder}
           </Placeholder>
           {React.Children.map(children, child => {
             return child && child.type !== Placeholder ? child : null
           })}
-        </components.ValueContainer>
+        </ValueContainer>
       )
     }
     return (
@@ -45,7 +45,7 @@ class SchoolSelector extends Component {
         isMulti={true}
         isSearchable={true}
         name="color"
-        options={data.map((d)=>({label: d.name, value: d.name}))}
+        options={generateOptions(data)}
         onChange={this.handleChange}
         value={schools}
         isDisabled={schools.length >= 3}
@@ -74,3 +74,15 @@ class SchoolSelector extends Component {
 SchoolSelector.contextType = DataContext
 
 export default SchoolSelector
+
+// Removes State Average from the options
+const generateOptions = (data) => {
+  return data.map(
+    (d) => {
+      if (d.name !== 'State Average') {
+        return {label: d.name, value: d.name}
+      }
+      return null
+    }
+  ).filter((n)=>(n))
+}
