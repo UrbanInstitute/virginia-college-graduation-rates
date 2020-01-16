562833bfca34eb20a7d2420a70b78aba47b9277c
Chartoff <bchartof@Informations-MacBook-Pro-2.local>
2020-01-16 16:12:44 -0500

    add basename to router

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.scss'
 
 function App() {
   return (
-    <Router history={browserHistory}>
+    <Router history={browserHistory} basename={"features/college-metrics-dashboard"}>
       <div className="App">
         <UrbanBar />
         <Switch>
