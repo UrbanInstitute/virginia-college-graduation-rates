30124a9945d2a7688bcf12fd874568661afbc721
Chartoff <bchartof@informaionsmbp2.urban.org>
2020-01-17 14:58:35 -0500

    Updates

--- precache-manifest.9362750ef85c9f528b60bcd27595651c.js
+++ precache-manifest.9362750ef85c9f528b60bcd27595651c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "807ea70be3e0e8481f3b662eedb15b2a",
+    "revision": "1fa081b0de1249e4d172c3e90081b9ee",
     "url": "/college-metrics-dashboard/index.html"
   },
   {
-    "revision": "bad533e6b6fb7e8ce2ff",
+    "revision": "d8cf3e6eff6063deb251",
     "url": "/college-metrics-dashboard/static/css/main.ee60a3cd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/college-metrics-dashboard/static/js/2.934b0d93.chunk.js.LICENSE"
   },
   {
-    "revision": "bad533e6b6fb7e8ce2ff",
-    "url": "/college-metrics-dashboard/static/js/main.703bae80.chunk.js"
+    "revision": "d8cf3e6eff6063deb251",
+    "url": "/college-metrics-dashboard/static/js/main.109f8712.chunk.js"
   },
   {
     "revision": "9e95bc4954c00637e729",
