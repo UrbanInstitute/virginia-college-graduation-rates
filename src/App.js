f7eb4cd54859683de621416ac7292b7a6cb277a9
Chartoff <bchartof@informaionsmbp2.urban.org>
2020-01-17 15:15:18 -0500

    hashrouter, no basename

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Router, Route, Switch} from 'react-router-dom'
+import {HashRouter, Route, Switch} from 'react-router-dom'
 
 import UrbanBar from './components/UrbanBar'
 import Home from './pages/Home'
@@ -10,14 +10,14 @@ import './App.scss'
 
 function App() {
   return (
-    <Router history={browserHistory} basename ='/'>
+    <HashRouter history={browserHistory}>
       <div className="App">
         <UrbanBar />
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
       </div>
-    </Router>
+    </HashRouter>
   )
 }
 
