a606bf8dc0bcd5e5d1edfcc87775876215613661
Chartoff <bchartof@informaionsmbp2.urban.org>
2020-01-17 15:06:38 -0500

    apache test build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Router, Route, Switch} from 'react-router-dom'
+import {Router, Route, Switch, HashRouter} from 'react-router-dom'
 
 import UrbanBar from './components/UrbanBar'
 import Home from './pages/Home'
@@ -10,14 +10,14 @@ import './App.scss'
 
 function App() {
   return (
-    <Router history={browserHistory} basename ='/'>
+    <HashRouter history={browserHistory} basename ='/'>
       <div className="App">
-        <UrbanBar />
+        <UrbanBar/>
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
       </div>
-    </Router>
+    </HashRouter>
   )
 }
 
