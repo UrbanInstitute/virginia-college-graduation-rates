62ca242ab5f1ac510a1deaee35277890719d3071
Chartoff <bchartof@informaionsmbp2.urban.org>
2020-01-17 14:46:20 -0500

    Updates

---
+++ precache-manifest.9d6657296fd019d8e48e65f28ff5a90c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8866391ed6a1da88d20b0b153c02d8c",
+    "url": "/college-metrics-staging/index.html"
+  },
+  {
+    "revision": "be76fb0b3accff0332ef",
+    "url": "/college-metrics-staging/static/css/main.ee60a3cd.chunk.css"
+  },
+  {
+    "revision": "77635318594531f6b6c8",
+    "url": "/college-metrics-staging/static/js/2.50caa232.chunk.js"
+  },
+  {
+    "revision": "eaaf359e6284af6bdda1d61285291759",
+    "url": "/college-metrics-staging/static/js/2.50caa232.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be76fb0b3accff0332ef",
+    "url": "/college-metrics-staging/static/js/main.1a86b31e.chunk.js"
+  },
+  {
+    "revision": "6a5047c5448a7af5080c",
+    "url": "/college-metrics-staging/static/js/runtime-main.db0f84da.js"
+  },
+  {
+    "revision": "ccb09460a8acdab571e10e4e484d4a2a",
+    "url": "/college-metrics-staging/static/media/book-icon.ccb09460.svg"
+  },
+  {
+    "revision": "024aaa44d8c23f4b16ae5a7d44f3eaa2",
+    "url": "/college-metrics-staging/static/media/cap-icon.024aaa44.svg"
+  },
+  {
+    "revision": "ce2ec0d54e351204a0d5e7a148fcf20d",
+    "url": "/college-metrics-staging/static/media/pencil-icon.ce2ec0d5.svg"
+  }
+]);
