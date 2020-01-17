f6ae181f53c7220fca6d518e4677a813c0320075
Chartoff <bchartof@informaionsmbp2.urban.org>
2020-01-17 15:02:52 -0500

    Updates

--- precache-manifest.0ec50b59263159b5392b469b523450d4.js
+++ precache-manifest.0ec50b59263159b5392b469b523450d4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d1ea2f0ba6723580c823ca67af094bad",
+    "revision": "d691aecf518006f1914227386f0c110e",
     "url": "/college-metrics-dashboard/index.html"
   },
   {
-    "revision": "1f6b492466bc1fba473c",
+    "revision": "211df2a13b9c90422a51",
     "url": "/college-metrics-dashboard/static/css/main.ee60a3cd.chunk.css"
   },
   {
-    "revision": "f66f48015d80d4151f4d",
-    "url": "/college-metrics-dashboard/static/js/2.a66ec1e1.chunk.js"
+    "revision": "3cd04759fe54405db29f",
+    "url": "/college-metrics-dashboard/static/js/2.934b0d93.chunk.js"
   },
   {
     "revision": "eaaf359e6284af6bdda1d61285291759",
-    "url": "/college-metrics-dashboard/static/js/2.a66ec1e1.chunk.js.LICENSE"
+    "url": "/college-metrics-dashboard/static/js/2.934b0d93.chunk.js.LICENSE"
   },
   {
-    "revision": "1f6b492466bc1fba473c",
-    "url": "/college-metrics-dashboard/static/js/main.398858f5.chunk.js"
+    "revision": "211df2a13b9c90422a51",
+    "url": "/college-metrics-dashboard/static/js/main.6aacb613.chunk.js"
   },
   {
     "revision": "9e95bc4954c00637e729",
